07acde450b86cfeab5bad22d4316d4f17c25ae78
Filipe Cifali Stangler <cifali.filipe@gmail.com>
2013-01-10 14:32:59 -0800

    Create crawler.py
    Tarara

---
+++ crawler.py
@@ -0,0 +1,17 @@
+# 
+# Use Requests to make the crawling of information from the websites.
+#
+import Requests
+
+class Crawler:
+
+    def __init__(self):
+        self.url = 'http://www.carris.com.br'
+        self.url_eptc = 'http://www.eptc.com.br/EPTC_Itinerarios/Linha.asp?cdEmp=3'
+
+    def get_url(self, url)
+        r = requests.get(self.url)
+        if r.status_code() == requests.codes.ok:
+            print r.json()
+        else:
+            print "check the url"
